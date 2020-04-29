d581d02efef741493ee250f0e83ee3c252ddea43
meeks21 <anthony.meeks3@gmail.com>
2020-04-29 02:05:14 -0700

    adds more jquery

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+// sticky navigation
 	$('.js--section-features').waypoint(function(direction) {
 		if (direction == "down") {
 			$('nav').addClass('sticky');
@@ -59,6 +59,30 @@ $('a[href*="#"]')
       }
     }
   });									  
-										 
+/********************* animations on scroll********/	
+	
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');					
+	}, {
+		offset: '50%'
+	});
+	
+	$('.js--wp-2').waypoint(function(direction){
+		$('.js--wp-2').addClass('animated fadeInUp');					
+	}, {
+		offset: '50%'
+	});
+	
+	$('.js--wp-3').waypoint(function(direction){
+		$('.js--wp-3').addClass('animated fadeIn');					
+	}, {
+		offset: '50%'
+	});
+	
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animated pulse');					
+	}, {
+		offset: '50%'
+	});
 
 });
